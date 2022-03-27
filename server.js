c70abcc476fb0e015913bffbd4502422d34d200e
Jose Roberto Rodriguez <josrobertrh@gmail.com>
2022-03-27 17:45:57 -0600

    commit configuraciones para app 3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/RickAndMorty'));
+app.use(express.static('./dist/rick-and-morty'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/RickAndMorty/'}),
+    res.sendFile('index.html', {root: 'dist/rick-and-morty/'}),
 );
 
 // Start the app by listening on the default Heroku port
